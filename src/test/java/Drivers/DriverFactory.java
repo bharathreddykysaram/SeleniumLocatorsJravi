4b348bf831da4948067b97fcac7a5a5d5711b8bc
bharat kysaram <bharath.kbr.reddy@example.com>
2024-12-18 17:35:04 +0530

    navigation methods in selenium

--- src/test/java/Drivers/DriverFactory.java
+++ src/test/java/Drivers/DriverFactory.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.Test;
 
 public class DriverFactory {
@@ -28,4 +29,16 @@ public class DriverFactory {
         driver.get("https://google.com");
         driver.quit();
     }
+
+
+    @Test
+    public void navigateTopageusingheadlessff(){
+        FirefoxOptions options=new FirefoxOptions();
+        options.addArguments("-headless");
+        WebDriver driver=new FirefoxDriver(options);
+        driver.get("https://google.com");
+        driver.quit();
+    }
+
+
 }
